63182f5da87e05b5206f09fa12841257c0fb8a14
sreelekhasandy <sreelekhasandy@192.168.0.3>
2019-06-07 12:51:14 +0530

    its my first change

---
+++ src/main/java/com/app/SpringInputExp1Application.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInputExp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInputExp1Application.class, args);
+		System.out.println("hiii");
+	}
+
+}
